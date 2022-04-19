877e30b4f61760f501898138007fda89ca9a34ed
com <x7788@naver.com>
2022-04-19 23:22:07 +0900

    💄 Home.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>App.js</h2>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/main" element={<Main />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      <div className="home_title">
+        <h1>Moment of Memory</h1>
+        <h3>: 기억의 순간</h3>
+      </div>
+      <button onClick={() => navigate('/main')}>▶ Start</button>
+      <h5>By. Choi-HyunHo</h5>
     </div>
   );
 };
