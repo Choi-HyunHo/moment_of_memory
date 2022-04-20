a3a60b49469b556d259daef0b4d68dd70ad4faf7
com <x7788@naver.com>
2022-04-20 17:51:42 +0900

    ✨ Main - 날짜 변경 기능

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import Button from '../components/Button';
 import Header from '../components/Header';
 
 const Main = () => {
+  const [curDate, setDate] = useState(new Date());
+
+  const headText = `${curDate.getFullYear()}. ${curDate.getMonth() + 1}`;
+
+  const onIncrease = () => {
+    setDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const onDecrease = () => {
+    setDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
     <div>
       <Header
-        left={<Button text={'<'} />}
-        text={2022}
-        right={<Button text={'>'} />}
+        left={<Button text={'<'} onClick={onDecrease} />}
+        text={headText}
+        right={<Button text={'>'} onClick={onIncrease} />}
       />
     </div>
   );
