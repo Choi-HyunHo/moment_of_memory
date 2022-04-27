818b582f699fd13e23af1019a1ad918c547de9ab
com <x7788@naver.com>
2022-04-27 17:20:56 +0900

    🚧 localStorage 저장 작업 중

--- src/App.js
+++ src/App.js
@@ -11,48 +11,48 @@ import Detail from './pages/Detail';
 export const AppDataContext = React.createContext();
 export const AppActivity = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    title: '라라랜드',
-    content1: '저녁 파티',
-    content2: '재밌다',
-    emotion: 5,
-    date: 1650519198811,
-  },
-  {
-    id: 2,
-    title: '미스터 션샤인',
-    content1: '저격 장면',
-    content2: '재밌다',
-    emotion: 4,
-    date: 1650519198812,
-  },
-  {
-    id: 3,
-    title: '스물다섯',
-    content1: '바다 놀러간 장면',
-    content2: '재밌다',
-    emotion: 3,
-    date: 1650519198813,
-  },
-  {
-    id: 4,
-    title: '도둑들',
-    content1: '저녁 파티',
-    content2: '재밌다',
-    emotion: 2,
-    date: 1650519198814,
-  },
-  {
-    id: 5,
-    title: '승리호',
-    content1: '저녁 파티',
-    content2: '재밌다',
-    emotion: 1,
-    date: 1650519198815,
-  },
-];
+// const dummyData = [
+//   {
+//     id: 1,
+//     title: '라라랜드',
+//     content1: '저녁 파티',
+//     content2: '재밌다',
+//     emotion: 5,
+//     date: 1650519198811,
+//   },
+//   {
+//     id: 2,
+//     title: '미스터 션샤인',
+//     content1: '저격 장면',
+//     content2: '재밌다',
+//     emotion: 4,
+//     date: 1650519198812,
+//   },
+//   {
+//     id: 3,
+//     title: '스물다섯',
+//     content1: '바다 놀러간 장면',
+//     content2: '재밌다',
+//     emotion: 3,
+//     date: 1650519198813,
+//   },
+//   {
+//     id: 4,
+//     title: '도둑들',
+//     content1: '저녁 파티',
+//     content2: '재밌다',
+//     emotion: 2,
+//     date: 1650519198814,
+//   },
+//   {
+//     id: 5,
+//     title: '승리호',
+//     content1: '저녁 파티',
+//     content2: '재밌다',
+//     emotion: 1,
+//     date: 1650519198815,
+//   },
+// ];
 
 const reducer = (state, action) => {
   let newState = [];
@@ -81,14 +81,16 @@ const reducer = (state, action) => {
       return state;
   }
 
+  localStorage.setItem('memory', JSON.stringify(newState));
+
   return newState;
 };
 
 function App() {
   // 전역으로 사용할 data
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
 
-  const dataId = useRef(6);
+  const dataId = useRef(0);
 
   // 추가
   const onCreate = (date, title, content1, content2, emotion) => {
